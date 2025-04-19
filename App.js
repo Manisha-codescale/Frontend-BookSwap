92ffa8ca2219d965b3bd49599eaca4417f4274dc
maneesha <manisha@codescale.lk>
2025-04-19 21:34:49 +0530

    Splash Screen

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
+import SignInScreen from './src/screens/SignInScreen';
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <View>
-      <Text>App</Text>
+      <Text>Welcome to the App</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
