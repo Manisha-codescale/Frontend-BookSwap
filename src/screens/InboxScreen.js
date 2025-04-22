5722789c9f6026e8da17746a281680dc0fcb347b
Roshmie <you@example.com>
2025-04-22 11:59:28 +0530

    update_changes_chats

---
+++ src/screens/InboxScreen.js
@@ -0,0 +1,13 @@
+import { View, Text, FlatList } from 'react-native'
+import React, {useState,useEffect} from 'react'
+
+
+const InboxScreen = () => {
+  return (
+      <FlatList>
+          <Text> Chat with :</Text>
+      </FlatList>
+  )
+}
+
+export default InboxScreen
